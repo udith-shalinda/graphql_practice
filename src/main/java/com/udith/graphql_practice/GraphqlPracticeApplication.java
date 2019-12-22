ada8cf46384d692de7c02454dbf38f8d8423dcb8
Udith <udithshalinda2@gmail.com>
2019-12-22 11:48:58 +0530

    first commit

---
+++ src/main/java/com/udith/graphql_practice/GraphqlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.udith.graphql_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udith/graphql_practice/GraphqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udith.graphql_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
